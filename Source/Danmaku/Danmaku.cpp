4ce62a4d9da7184aa8b00ddf55d844889471decf
Huy <huyrse@gmail.com>
2024-01-06 03:28:24 -0600

    Initial commit

---
+++ Source/Danmaku/Danmaku.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Danmaku.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Danmaku, "Danmaku" );
